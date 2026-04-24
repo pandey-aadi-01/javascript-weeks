b9c5a7f9ade15b93c0f61a4f8b0e7ca9e4337b67
AdityaPandey053 <aadipandey921@gmail.com>
2026-04-24 14:05:19 +0530

    counter-async-js

--- week-3/01-async-js/2-counter.js
+++ week-3/01-async-js/2-counter.js
@@ -4,4 +4,31 @@
 
 // (Hint: setTimeout)
 
+let counnt = 0;
 
+const runcode = () => {
+  counnt++;
+  console.log("print :", counnt)
+
+  if (counnt === 6) {
+    console.log("end here")
+    return;
+  }
+  setTimeout(runcode, 1000)
+}
+
+runcode();
+
+let counter = 0;
+
+function runcd() {
+  counter = counter + 1;
+  console.log(counter);
+
+  if (counter === 13){
+    console.log(" work done")
+    return;
+  }
+  setTimeout(runcd, 1000)
+}
+runcd();
---
+++ week-3/map or filter/index.js
@@ -0,0 +1,80 @@
+// Q1. Double each number (map)
+// Input: [1, 2, 3]
+
+const arr = [1, 2, 3];
+
+const newarr = arr.map(n => n * 2);
+
+console.log(newarr);
+
+// function
+
+
+const num = [1, 2, 3, 6, 9];
+
+
+const fun = num.map(function (n) {
+  return n * 2;
+})
+
+console.log(fun)
+
+
+const num1= [3, 6, 9];
+
+const funs = function (n) {
+  return n * 2;
+}
+
+const ans = num1.map(funs);
+console.log(ans)
+
+// Q2.Convert numbers to strings(map)
+// Input: [1, 2, 3]
+// Output: ["1", "2", "3"]
+
+const str = [1, 2, 3];
+
+const tostr = str.map(n => n.toString());
+
+console.log(tostr)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q3.Keep only even numbers(filter)
+// Input: [1, 2, 3, 4]
+// Output: [2, 4]
