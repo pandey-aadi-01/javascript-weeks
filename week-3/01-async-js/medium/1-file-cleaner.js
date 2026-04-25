99d6c918291c5631f1dca1ed30f103fe0b2e3a2e
AdityaPandey053 <aadipandey921@gmail.com>
2026-04-25 15:29:54 +0530

    medium 1-file-cleaner.js done

--- week-3/01-async-js/medium/1-file-cleaner.js
+++ week-3/01-async-js/medium/1-file-cleaner.js
@@ -12,5 +12,24 @@
 // hello world my name is raman
 // ```
 
+const fs = require("fs");
 
+fs.readFile("a.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("err catch :", err);
+  }
+  const cleaned = data.replace(/\s+/g, " ");
+  console.log("done!", cleaned);
+})
+
+
+
+
+fs.readFile("b.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("the output", err)
+  }
+  const clean = data.replace(/\s+/g  ," ");
+  console.log("trim successfully: " ,clean);
+})
 
