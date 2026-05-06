ddaa8cf12d3effe0b4acad103579e23c0f27e825
AdityaPandey053 <aadipandey921@gmail.com>
2026-05-06 23:49:12 +0530

    first commit

---
+++ c.cpp
@@ -0,0 +1,90 @@
+// A - Hello World
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World";
+}
+
+
+// B - Print First 5 Alphabets
+/*Your task is to print the first five letters of the English alphabet.
+
+Input
+There is no input for this problem.
+
+Output
+Print the first five uppercase English letters, one per line.
+
+Example
+InputCopy
+There is no input.
+OutputCopy
+A
+B
+C
+D
+E*/
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"A" <<endl;
+    cout<<"B"<<endl;
+    cout<<"C"<<endl;
+    cout<<"D"<<endl;
+    cout<<"E"<<endl;
+}
+
+
+// C - Triangle
+/*Your task is to print a right-angled triangle pattern using asterisks (*).
+
+Input
+There is no input for this problem.
+
+Output
+Print the following pattern exactly as shown:
+
+
+*****
+****
+***
+**
+**/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "*****" << endl;
+    cout << "****" << endl;
+    cout << "***" << endl;
+    cout << "**" << endl;
+    cout << "*" << endl;
+}
+
+// D - Print Z
+/*Your task is to print the letter Z using asterisks (*) as shown below.
+
+Input
+There is no input for this problem.
+
+Output
+Print the following pattern exactly as shown:
+
+
+*****
+   *
+  *
+ *
+*****
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "*****" << endl;
+    cout << "   *" << endl;
+    cout << "  *" << endl;
+    cout << " *" << endl;
+    cout << "*****" << endl;
+}
