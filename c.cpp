b2514e595034411205a11f78eafc08731c9ec8a6
AdityaPandey053 <aadipandey921@gmail.com>
2026-05-07 00:03:28 +0530

    c++

--- c.cpp
+++ c.cpp
@@ -186,3 +186,87 @@ int main(){
     cout<<"Area = "<<length*breadth <<endl;
     cout<<"Perimeter = "<<2*(length+breadth)<<endl;
 }
+/*
+G - Print Table of N
+Input
+A single integer N
+ (1≤N≤100)
+.
+
+Output
+Print the table of N
+ in the format shown in the example below.
+
+Example
+InputCopy
+6
+OutputCopy
+6 * 1 = 6
+6 * 2 = 12
+6 * 3 = 18
+6 * 4 = 24
+6 * 5 = 30
+6 * 6 = 36
+6 * 7 = 42
+6 * 8 = 48
+6 * 9 = 54
+6 * 10 = 60
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= 10; i++) {
+        cout << N << " * " << i << " = " << N * i << endl;
+    }
+
+    return 0;
+}
+
+
+/*You are given two integers N
+ and M
+. Your task is to compute and print the results of the following operations:
+
+N+M
+N−M
+N×M
+N÷M
+ (integer division)
+NmodM
+Input
+A single line containing two integers N
+ and M
+ (1≤N,M≤1000)
+.
+
+Output
+Print five lines in the format as shown in the example.
+
+Example
+Input
+6 4
+Output
+6 + 4 = 10
+6 - 4 = 2
+6 * 4 = 24
+6 / 4 = 1
+6 % 4 = 2
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int N,M;
+cin>>N>>M;
+cout<<N<<" + "<<M<<" = "<<(N+M)<<endl;
+cout<<N<<" - "<<M<<" = "<<N-M<<endl;
+cout<<N<<" * "<<M<<" = "<<N*M<<endl;
+cout<<N<<" / "<<M<<" = "<<N/M<<endl;
+cout<<N<<" % "<<M<<" = "<<N%M<<endl;
+}
