e8530fe8e8602d5a4075398ea7a7f9dd889a0d3f
AdityaPandey053 <aadipandey921@gmail.com>
2026-05-07 23:18:06 +0530

    c++

--- c.cpp
+++ c.cpp
@@ -322,3 +322,52 @@ int main()
 
 
 }
+
+/*
+H1 - Calculator
+You are given two integers N
+ and M
+. Your task is to compute and print the results of the following operations:
+
+N+M
+N−M
+N×M
+N÷M
+ (integer division)
+NmodM
+Input
+A single line containing two integers N
+ and M
+ (1≤N,M≤1000)
+.
+
+Output
+Print five lines in the format as shown in the example.
+
+Example
+InputCopy
+6 4
+OutputCopy
+6 + 4 = 10
+6 - 4 = 2
+6 * 4 = 24
+6 / 4 = 1
+6 % 4 = 2
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int N,M;
+cin>>N>>M;
+cout<<N<<" + "<<M<<" = "<<(N+M)<<endl;
+cout<<N<<" - "<<M<<" = "<<N-M<<endl;
+cout<<N<<" * "<<M<<" = "<<N*M<<endl;
+cout<<N<<" / "<<M<<" = "<<N/M<<endl;
+cout<<N<<" % "<<M<<" = "<<N%M<<endl;
+}
+ 
