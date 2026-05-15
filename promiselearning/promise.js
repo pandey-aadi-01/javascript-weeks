393506ad8967d169283110b7593c65513fd5c31e
AdityaPandey053 <aadipandey921@gmail.com>
2026-05-15 17:33:13 +0530

    promise slovng

---
+++ promiselearning/promise.js
@@ -0,0 +1,82 @@
+//
+// console.log("A");
+
+// console.log("B");
+
+// console.log("C");
+
+// console.log("Hi");
+
+// console.log("Adi");
+
+// console.log("1");
+
+
+
+// setTimeout(() => {
+//   console.log("2");
+// }, 1000);
+
+// console.log("3");
+
+// console.log("Start");
+
+// setTimeout(() => {
+//   console.log("Hello");
+// }, 2000);
+
+// console.log("End");
+
+// setTimeout(() => {
+//   console.log("Timer Done");
+// }, 3000);
+
+
+// setTimeout(() => {
+//   console.log("This will run after 0 seconds");
+// }, 0);
+
+// console.log("addi");
+
+// function one(){
+//   two()
+// }
+
+// function two(){
+//   console.log("two");
+// }
+// one();
+
+// function one() {
+//   console.log("one");
+//   two()
+// }
+
+// function two() {
+//   console.log("two");
+// }
+// one();
+
+// function one() {
+
+//   two();
+//   console.log("one");
+// }
+
+// function two() {
+//   console.log("two");
+// }
+// one();
+
+
+function a() {
+  console.log("A");
+
+}
+
+function b() {
+  a();
+}
+
+b();
+ 
